6f2faafcbdc6edb8566b6c2fd03ee4aa0cba651e
harsh-tank <65907699+harsh-tank@users.noreply.github.com>
2022-02-21 13:42:34 +0530

    Delete 2nd Submission directory

--- 2nd Submission/Login/login.repository.ts
+++
@@ -1,9 +0,0 @@
-import {db} from "../models/index"
-import {User} from "../models/user"
-
-export class LoginRepository{
-
-    public async getUserByEmail(userEmail:string):Promise<User|null>{
-        return db.User.findOne({where:{Email:userEmail}});
-    }
-}
