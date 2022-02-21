f6c476e621964df95a5e08917de3bc55a97028ea
harsh-tank <65907699+harsh-tank@users.noreply.github.com>
2022-02-21 13:54:05 +0530

    Add files via upload

---
+++ routes/bookservice.ts
@@ -0,0 +1,38 @@
+import express from 'express';
+import { celebrate } from 'celebrate';
+
+import { LoginController } from "../Login/login.controller";
+import { LoginSchema } from "../Login/login.model";
+import { LoginRepository } from "../Login/login.repository";
+import { LoginService } from "../Login/login.service";
+
+import { BookServiceRepository } from "../Book_Service/bookservice.repository";
+import { BookServiceController } from "../Book_Service/bookservice.controller";
+import { BookService } from "../Book_Service/bookservice.service";
+import { BookServiceSchema } from "../Book_Service/bookservice.model";
+
+const router: express.Router = express.Router();
+
+const loginRepo:LoginRepository = new LoginRepository();
+const loginService:LoginService = new LoginService(loginRepo);
+const loginController:LoginController = new LoginController(loginService)
+
+const bookRepo: BookServiceRepository = new BookServiceRepository();
+const bookService: BookService= new BookService(bookRepo);
+const bookController: BookServiceController = new BookServiceController(bookService);
+
+const { zipcode_match, U_Add, Create_Ser} = BookServiceSchema;
+
+router.post('/confirm-zipcode',celebrate(zipcode_match),loginController.authenticateToken,bookController.confirmServiceAvailable);
+
+router.post('/create-ser_request', celebrate(Create_Ser),loginController.authenticateToken,bookController.decodeToken, bookController.CreateServiceRequest);
+
+router.post('/user-address', celebrate(U_Add),loginController.authenticateToken,bookController.createUserAddress);
+
+router.get('/user-addresses', loginController.authenticateToken,bookController.getUserAddresses);
+
+router.post('/create-favorite-blocked', loginController.authenticateToken,bookController.createFavoriteAndBlocked);
+
+router.get('/create-favorite-blocked', loginController.authenticateToken,bookController.getFavoriteAndBlocked);
+
+export = router;
