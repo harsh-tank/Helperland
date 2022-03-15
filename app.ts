52cd4d093099e473e301f4a44d0b5f4029d1a23e
harsh-tank <65907699+harsh-tank@users.noreply.github.com>
2022-03-15 11:10:40 +0530

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,63 @@
+import express from "express";
+import router from "./routes/index";
+import multer from "multer";
+import cors from "cors";
+import {sequelize} from "./models";
+import  swaggerJSDoc from "swagger-jsdoc";
+import  swaggerUI from "swagger-ui-express";
+import cookieParser from "cookie-parser";
+
+const corsOptions = {
+    origin: `http://localhost:${process.env.PORT}`
+  };
+
+  const app = express();
+
+  const swaggerOption={
+    definition:{
+        openapi:'3.0.0',
+        info:{
+            title:'Helperland APIs',
+            version: '1.0.0',
+            description:'Helperland APIs',
+            contact:{
+                name:'Harsh Tank',
+                email:'harshptank2212@gmail.com'
+            },
+            servers:[
+                {
+                url: "http://locslhost:3000"
+                }
+            ]
+        }
+    },
+    apis:["routes.ts"]
+}
+const swaggerDocs = swaggerJSDoc(swaggerOption);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
+  
+  app.use(cors(corsOptions));
+
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: false}));
+  app.use(multer({dest:'uploadFiles'}).single('file'));
+  require('dotenv').config();
+
+app.use('/',router);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server rocking at ${process.env.PORT}`)
+  sequelize.authenticate().then(async() => {
+      console.log("database connected");
+
+      try {
+          await sequelize.sync();
+      } catch (error) {
+          console.log(error)
+      }
+
+  }).catch( (e: any) => {
+      console.log(e.message)
+  })
+})
