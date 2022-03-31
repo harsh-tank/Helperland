3379bfc2d7f7e024b10b1e2b9d6c25dbb630e140
harsh-tank <65907699+harsh-tank@users.noreply.github.com>
2022-03-31 14:43:59 +0530

    Delete public directory

--- public/Admin Screen/User Management/usermanagement.model.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const celebrate_1 = require("celebrate");
-const params1 = {
-    id: celebrate_1.Joi.number().integer().required().description('User ID')
-};
