670c906222d6e2dbd6d2b0987d2eb3db37f52eb0
harsh-tank <65907699+harsh-tank@users.noreply.github.com>
2022-03-31 14:48:29 +0530

    Add files via upload

---
+++ Forgot Password/forgotpassword.service.ts
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/user";
+import { ForgotPasswordRepository } from "./forgotpassword.repository";
+require("dotenv").config();
+
+  export class ForgotPasswordService {
+  public constructor(private readonly forgotpasswordRepository: ForgotPasswordRepository) {
+    this.forgotpasswordRepository = forgotpasswordRepository;
+  }
+
+  public async getUserByEmail(userEmail:string):Promise<User|null>{
+    return this.forgotpasswordRepository.getUserByEmail(userEmail);
+  }
+
+
+  public createData(userEmail:string, link:string): typeof temp{
+    const temp = {
+        from: 'support_helperland@gmail.com',
+        to: userEmail,
+        subject: 'Reset Password link',
+        html: `<h2>Please click here to reset your password</h2>
+              <a href="${process.env.Company_URL}/reset/${link}">Please click here to reset your password</a>`
+    }
+    return temp;
+  }
+
+  public createToken(userEmail:string):string{
+      const token = jwt.sign({userEmail},process.env.FORGOT_PASSWORD!,{expiresIn:'1h'});
+      return token;
+  }
+
+  public async updateUser(userPassword:string, userEmail:string):Promise<[number, User[]]>{
+    return this.forgotpasswordRepository.updateUser(userPassword, userEmail);
+}
+}
