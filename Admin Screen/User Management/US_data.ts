3687e786212d5558c5451bf6847f61a84825f5b6
harsh-tank <65907699+harsh-tank@users.noreply.github.com>
2022-03-31 14:41:22 +0530

    Delete Admin Screen directory

--- Admin Screen/User Management/US_data.ts
+++
@@ -1,9 +0,0 @@
-export type Print_data= {
-            UserId: number,
-            Name: string,
-            DateOfRegistration:string,
-            UserType:string;
-            Phone: string,
-            PostalCode : string | null,
-            Status: boolean
-        }
--- Admin Screen/User Management/usermanagement.model.ts
+++
@@ -1,4 +0,0 @@
-import {Joi} from "celebrate"
-const params1: object = {
-    id: Joi.number().integer().required().description('User ID')
-};
